cf63a63ecf38446ca09cdc1ec03bd8e3eec4c8f1
ZarakiLancelot <kenpachi2783@gmail.com>
2023-04-09 14:38:38 -0600

    hotfix links to social media

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -3,13 +3,13 @@ import styles from './SwitchTheme.module.css';
 const SocialMedia = () => {
   return (
     <div className={styles.btns}>
-      <a href="https://www.linkedin.com/in/edwin-einsen-vasquez-velasquez/">
+      <a href="https://www.linkedin.com/in/edwin-einsen-vasquez-velasquez/" rel="noopener noreferrer external">
         <img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" />
       </a>
-      <a href="https://github.com/ZarakiLancelot/grocery-list.git">
+      <a href="https://github.com/ZarakiLancelot/grocery-list.git" rel="noopener noreferrer external">
         <img src="https://img.icons8.com/fluent/48/000000/github.png" alt="GitHub" />
       </a>
-      <a href="https://twitter.com/DevChapin">
+      <a href="https://twitter.com/DevChapin" rel="noopener noreferrer external">
         <img src="https://img.icons8.com/fluent/48/000000/twitter.png" alt="Twitter" />
       </a>
       <div>
